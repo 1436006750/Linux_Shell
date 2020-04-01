b6aae7c8575499b24f75460c2bd970ab528febe4
xiang.zheng <1436006750@qq.com>
2020-04-01 19:38:15 +0800

    Shell

--- 2.0_Shell脚本编程/0.2_Shell的条件执行/01_条件测试.sh
+++ 2.0_Shell脚本编程/0.2_Shell的条件执行/01_条件测试.sh
@@ -1,3 +1,25 @@
 #!/bin/bash 
 
+a=2
 
+
+
+if [ $a -eq 1 ];then
+  echo 1
+else
+  echo 2
+fi
+
+
+
+echo '-------------------------------------'
+
+
+case $a in
+1)
+  echo 1
+  ;;
+2)
+  echo 2
+  ;;
+esac
